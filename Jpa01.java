e003e68c812f45f495411da62bc2c41f581cc914
ls12t55 <161402022+ls12t55@users.noreply.github.com>
2024-04-02 00:31:12 +0800

    Create Jpa01.java

---
+++ Jpa01.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class JPA01 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Input:");
+        int n = scanner.nextInt();
+
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        System.out.println(1 + " + ... + " + n + " = " + sum);
+    }
+}
